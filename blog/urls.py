8a3baf719bba6604179c952c06ca541f8abca362
toma86hawk <take09stjim.ikes@gmial.com>
2019-10-02 16:20:58 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),   #post_listという名前にしたビューをルートURLに設定 ' 'はhttp..8000/ドメインを無視する
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),   #投稿の詳細ページのurlを設定
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Post  #クエリセットを使ってポストの一覧を操作したい
 from django.utils import timezone  #クエリセットでtimezoneを使用
+from django.shortcuts import render, get_object_or_404
 # Create your views here.
 
 #urls.pyからリクエスト時にmodels.pyのデータをpost_list.htmlに渡す
@@ -8,5 +9,9 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') #クエリセットを参照するメソッド
     return render(request, 'blog/post_list.html', {'posts': posts})   #renderでrequestを引数に受け取り、post_list.htmlを組み立てる {}は指定した情報をテンプレートが表示
 
+#ビューを追加する
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
 
 
